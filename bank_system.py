6f8c06d7cae9cd2881b3bf1b4cb9f0cb555c3edf
danielesousas <145569612+danielesousas@users.noreply.github.com>
2023-12-20 10:29:12 -0300

    update

--- bank_system.py
+++ bank_system.py
@@ -11,6 +11,7 @@ menu = """
 
 saldo = 0
 limite = 500
+extrato = ""
 numero_saques = 0
 limite_saques = 3
 
@@ -22,11 +23,10 @@ while True:
     if opcao == "d":
         valor = float(input("Informe o valor do depósito: "))
 
-
         if valor > 0:
             saldo += valor
-            extrato = f"Depósito: R$ {valor:.2f}∖n"
-
+            extrato += f'Deposito: R$ {valor: .2f}\n'
+        
         else:
             print("Operação falhou! O valor informado é inválido.")
 
@@ -49,4 +49,22 @@ while True:
             print("Operação falhou! Número máximo de saques excedido.")
 
         elif valor > 0:
-            
+            saldo -= valor
+            extrato = f'Saque: R$ {valor:.f}∖n'
+            numero_saques += 1
+
+        else:
+            print("Operação falhou! O valor informado é inválido.")
+
+    elif opcao == "e":
+        print("∖n***************EXTRATO****************")
+        print("Não foram realizadas movimentações.")
+        print(f"∖n Saldo: R$ {valor:.2f}∖n")
+        print("***************************************")
+
+    elif opcao == "q":
+        break
+
+
+    else:
+        print("Operação inválida, por favor selecione novamente a operação desejada.")        
